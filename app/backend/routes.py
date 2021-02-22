23bafda0d563114aeef34821324c99e38f721995
Cameron Bahl <cameron-bahl@uiowa.edu>
2021-02-21 23:02:45 -0600

    Set up the basics and initialized the database

---
+++ app/backend/routes.py
@@ -0,0 +1,19 @@
+from app import app
+from flask import render_template, flash, redirect, url_for
+from app.backend.forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Test'}
+    return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}'.format(form.username.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
