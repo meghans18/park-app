415e7bc2e3c04cea14f6d2c312883dfa5d0cadaf
Meghan Saak <saakmeghan@gmail.com>
2021-02-21 19:02:30 -0600

    setting up actions for registering user, logging in, and logging out

--- client/src/store/modules/auth.js
+++ client/src/store/modules/auth.js
@@ -1,12 +1,26 @@
-//import axios from 'axios';
+import axios from 'axios';
 const state = {
-
+    user: null,
 };
 const getters = {
 
 };
 const actions = {
-
+    async Register({dispatch}, form) {
+        await axios.post('register', form)
+        let UserForm = new FormData()
+        UserForm.append('username', form.username)
+        UserForm.append('password', form.password)
+        await dispatch('LogIn', UserForm)
+    },
+    async LogIn({commit}, User) {
+        await axios.post('login', User)
+        await commit('setUser', User.get('username'))
+    },
+    async LogOut({commit}){
+        let user = null
+        commit('logOut', user)
+    }
 };
 const mutations = {
 
